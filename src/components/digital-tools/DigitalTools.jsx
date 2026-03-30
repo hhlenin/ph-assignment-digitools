1ccb75b98f02ba601c5281e0da63fc35572241bb
Hasibul Lenin <hasibul.lenin@gmail.com>
2026-03-30 07:45:54 -0400

    products card complete

--- src/components/digital-tools/DigitalTools.jsx
+++ src/components/digital-tools/DigitalTools.jsx
@@ -1,30 +1,29 @@
 import React, {useState} from 'react';
+import Product from "./products/Product.jsx";
 
 const DigitalTools = () => {
 
     const [products, setProducts] = useState(true);
-    const setProductButtonActive = () => {
-        !products && (setProducts(!products), setCart(true));
-    }
-    const [cart, setCart] = useState(false);
-    const setCartButtonActive = () => {
-        setCart(!cart)
 
-    }
     return (
-        <div className="container">
-            <div className="p-32">
-                <div className="text-center space-y-4">
-                    <h3 className="text-5xl font-extrabold">Premium Digital Tools</h3>
-                    <p className="font-normal text-[#627382] lea">Choose from our curated collection of premium digital products designed <br/> to boost your productivity and creativity.</p>
-                </div>
-                <div>
-                    <button onClick={() => setProductButtonActive()} className={products && 'btn btn-primary'}>Products</button>
-                    <button onClick={() => setCartButtonActive()} className={cart && 'btn btn-primary'}>Cart (0)</button>
+        <main className="container">
+            <div className="p-32 justify-center">
+                <div className="space-y-4 flex flex-col justify-center items-center">
+                    <div className="text-center space-y-4">
+                        <h3 className="text-5xl font-extrabold">Premium Digital Tools</h3>
+                        <p className="font-normal text-[#627382] lea">Choose from our curated collection of premium digital products designed <br/> to boost your productivity and creativity.</p>
+                    </div>
+                    <span className="flex w-fit justify-center gap-3 p-0.5 border border-[#627382] rounded-4xl">
+                        <button onClick={() => setProducts(!products)} className={`${products && 'text-white btn bg-linear-to-r from-[#4f39f6] to-[#9514fa]'} rounded-4xl`}>Products</button>
+                        <button onClick={() => setProducts(!products)} className={`${!products && 'text-white btn bg-linear-to-r from-[#4f39f6] to-[#9514fa]'} rounded-4xl`}>Cart (0)</button>
+                    </span>
+                    <div className="pt-10">
+                        <Product></Product>
+                    </div>
                 </div>
             </div>
 
-        </div>
+        </main>
     );
 };
 
---
+++ src/components/digital-tools/products/Product.jsx
@@ -0,0 +1,236 @@
+import React from 'react';
+import randIng from "../../../assets/products/design-tool.png"
+
+const Product = () => {
+    return (
+        <div className="grid grid-cols-3 gap-7">
+            <div className="card bg-base-100 shadow-sm">
+                <div className="card-body space-y-4">
+                    <div className="text-right">
+                        <span className="text-right badge badge-soft badge-xs badge-warning">Most Popular</span>
+                    </div>
+                    <div className="w-15 h-15 border border-[#627382] rounded-full flex justify-center items-center">
+                        <img src={randIng} alt="" className="w-8 h-8"/>
+                    </div>
+                    <h2 className="text-2xl font-bold">Premium</h2>
+                    <p>Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.</p>
+
+                    <span className="text-xl font-bold">$29<span className="text-[#627382]">/mo</span></span>
+                    <ul className=" flex flex-col gap-2 text-xs">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>High-resolution image generation</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Customizable style templates</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Batch processing capabilities</span>
+                        </li>
+
+                    </ul>
+                    <div className="mt-6">
+                        <button className="text-white bg-linear-to-r from-[#4f39f6] to-[#9514fa] btn btn-block rounded-4xl">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 shadow-sm">
+                <div className="card-body space-y-4">
+                    <div className="text-right">
+                        <span className="text-right badge badge-soft badge-xs badge-warning">Most Popular</span>
+                    </div>
+                    <div className="w-15 h-15 border border-[#627382] rounded-full flex justify-center items-center">
+                        <img src={randIng} alt="" className="w-8 h-8"/>
+                    </div>
+                    <h2 className="text-2xl font-bold">Premium</h2>
+                    <p>Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.</p>
+
+                    <span className="text-xl font-bold">$29<span className="text-[#627382]">/mo</span></span>
+                    <ul className=" flex flex-col gap-2 text-xs">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>High-resolution image generation</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Customizable style templates</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Batch processing capabilities</span>
+                        </li>
+
+                    </ul>
+                    <div className="mt-6">
+                        <button className="text-white bg-linear-to-r from-[#4f39f6] to-[#9514fa] btn btn-block rounded-4xl">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 shadow-sm">
+                <div className="card-body space-y-4">
+                    <div className="text-right">
+                        <span className="text-right badge badge-soft badge-xs badge-warning">Most Popular</span>
+                    </div>
+                    <div className="w-15 h-15 border border-[#627382] rounded-full flex justify-center items-center">
+                        <img src={randIng} alt="" className="w-8 h-8"/>
+                    </div>
+                    <h2 className="text-2xl font-bold">Premium</h2>
+                    <p>Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.</p>
+
+                    <span className="text-xl font-bold">$29<span className="text-[#627382]">/mo</span></span>
+                    <ul className=" flex flex-col gap-2 text-xs">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>High-resolution image generation</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Customizable style templates</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Batch processing capabilities</span>
+                        </li>
+
+                    </ul>
+                    <div className="mt-6">
+                        <button className="text-white bg-linear-to-r from-[#4f39f6] to-[#9514fa] btn btn-block rounded-4xl">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 shadow-sm">
+                <div className="card-body space-y-4">
+                    <div className="text-right">
+                        <span className="text-right badge badge-soft badge-xs badge-warning">Most Popular</span>
+                    </div>
+                    <div className="w-15 h-15 border border-[#627382] rounded-full flex justify-center items-center">
+                        <img src={randIng} alt="" className="w-8 h-8"/>
+                    </div>
+                    <h2 className="text-2xl font-bold">Premium</h2>
+                    <p>Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.</p>
+
+                    <span className="text-xl font-bold">$29<span className="text-[#627382]">/mo</span></span>
+                    <ul className=" flex flex-col gap-2 text-xs">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>High-resolution image generation</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Customizable style templates</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Batch processing capabilities</span>
+                        </li>
+
+                    </ul>
+                    <div className="mt-6">
+                        <button className="text-white bg-linear-to-r from-[#4f39f6] to-[#9514fa] btn btn-block rounded-4xl">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 shadow-sm">
+                <div className="card-body space-y-4">
+                    <div className="text-right">
+                        <span className="text-right badge badge-soft badge-xs badge-warning">Most Popular</span>
+                    </div>
+                    <div className="w-15 h-15 border border-[#627382] rounded-full flex justify-center items-center">
+                        <img src={randIng} alt="" className="w-8 h-8"/>
+                    </div>
+                    <h2 className="text-2xl font-bold">Premium</h2>
+                    <p>Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.</p>
+
+                    <span className="text-xl font-bold">$29<span className="text-[#627382]">/mo</span></span>
+                    <ul className=" flex flex-col gap-2 text-xs">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>High-resolution image generation</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Customizable style templates</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Batch processing capabilities</span>
+                        </li>
+
+                    </ul>
+                    <div className="mt-6">
+                        <button className="text-white bg-linear-to-r from-[#4f39f6] to-[#9514fa] btn btn-block rounded-4xl">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 shadow-sm">
+                <div className="card-body space-y-4">
+                    <div className="text-right">
+                        <span className="text-right badge badge-soft badge-xs badge-warning">Most Popular</span>
+                    </div>
+                    <div className="w-15 h-15 border border-[#627382] rounded-full flex justify-center items-center">
+                        <img src={randIng} alt="" className="w-8 h-8"/>
+                    </div>
+                    <h2 className="text-2xl font-bold">Premium</h2>
+                    <p>Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.</p>
+
+                    <span className="text-xl font-bold">$29<span className="text-[#627382]">/mo</span></span>
+                    <ul className=" flex flex-col gap-2 text-xs">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>High-resolution image generation</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Customizable style templates</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Batch processing capabilities</span>
+                        </li>
+
+                    </ul>
+                    <div className="mt-6">
+                        <button className="text-white bg-linear-to-r from-[#4f39f6] to-[#9514fa] btn btn-block rounded-4xl">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 shadow-sm">
+                <div className="card-body space-y-4">
+                    <div className="text-right">
+                        <span className="text-right badge badge-soft badge-xs badge-warning">Most Popular</span>
+                    </div>
+                    <div className="w-15 h-15 border border-[#627382] rounded-full flex justify-center items-center">
+                        <img src={randIng} alt="" className="w-8 h-8"/>
+                    </div>
+                    <h2 className="text-2xl font-bold">Premium</h2>
+                    <p>Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.</p>
+
+                    <span className="text-xl font-bold">$29<span className="text-[#627382]">/mo</span></span>
+                    <ul className=" flex flex-col gap-2 text-xs">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>High-resolution image generation</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Customizable style templates</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>Batch processing capabilities</span>
+                        </li>
+
+                    </ul>
+                    <div className="mt-6">
+                        <button className="text-white bg-linear-to-r from-[#4f39f6] to-[#9514fa] btn btn-block rounded-4xl">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Product;
