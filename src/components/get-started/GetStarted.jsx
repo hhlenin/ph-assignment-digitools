a985938e26bc460d1ce7ed5d497a17065a37cf8f
Hasibul Lenin <hasibul.lenin@gmail.com>
2026-03-31 08:56:15 -0400

    fix:get started design

--- src/components/get-started/GetStarted.jsx
+++ src/components/get-started/GetStarted.jsx
@@ -13,36 +13,43 @@ const GetStarted = () => {
                 </div>
                 <div className="grid grid-cols-3 gap-7">
                     <div className="card shadow-md">
-                        <div className="card-body space-y-4 items-center text-center">
-                            <div className="">
+                        <div className="card-body space-y-4 text-center">
+                            <div className="flex justify-end">
                                 <span className="text-right badge text-white h-10 w-10 rounded-full bg-linear-to-r from-[rgb(79,57,246)] to-[rgb(149,20,250)] font-bold">01</span>
                             </div>
-                            <div className="bg-linear-to-r from-[rgba(79,57,246,0.1)] to-[rgba(149,20,250,0.1)] w-25 h-25 rounded-full flex justify-center items-center">
-                                <img src={userImg} alt="" className=""/>
+                            <div className="flex justify-center">
+                                <div className="bg-linear-to-r from-[rgba(79,57,246,0.1)] to-[rgba(149,20,250,0.1)] w-25 h-25 rounded-full flex justify-center items-center">
+                                    <img src={userImg} alt="" className=""/>
+                                </div>
                             </div>
+
                             <h4 className="text-2xl font-bold">Create Account</h4>
                             <p className="pb-22 text-[#627382]">Sign up for free in seconds. No credit card required to get started.</p>
                         </div>
                     </div>
                     <div className="card shadow-md">
-                        <div className="card-body space-y-4 items-center text-center">
-                            <div className="">
+                        <div className="card-body space-y-4 text-center">
+                            <div className="flex justify-end">
                                 <span className="text-right badge text-white h-10 w-10 rounded-full bg-linear-to-r from-[rgb(79,57,246)] to-[rgb(149,20,250)] font-bold">02</span>
                             </div>
-                            <div className="bg-linear-to-r from-[rgba(79,57,246,0.1)] to-[rgba(149,20,250,0.1)] w-25 h-25 rounded-full flex justify-center items-center">
-                                <img src={productImg} alt="" className=""/>
+                            <div className="flex justify-center">
+                                <div className="bg-linear-to-r from-[rgba(79,57,246,0.1)] to-[rgba(149,20,250,0.1)] w-25 h-25 rounded-full flex justify-center items-center">
+                                    <img src={productImg} alt="" className=""/>
+                                </div>
                             </div>
                             <h4 className="text-2xl font-bold">Choose Products</h4>
                             <p className="pb-22 text-[#627382]">Browse our catalog and select the tools that fit your needs.</p>
                         </div>
                     </div>
                     <div className="card shadow-md">
-                        <div className="card-body space-y-4 items-center text-center">
-                            <div className="flex">
-                                <p className="text-right badge text-white h-10 w-10 rounded-full bg-linear-to-r from-[rgb(79,57,246)] to-[rgb(149,20,250)] font-bold">03</p>
+                        <div className="card-body space-y-4 text-center">
+                            <div className="flex justify-end">
+                                <span className="text-right badge text-white h-10 w-10 rounded-full bg-linear-to-r from-[rgb(79,57,246)] to-[rgb(149,20,250)] font-bold">03</span>
                             </div>
-                            <div className="bg-linear-to-r from-[rgba(79,57,246,0.1)] to-[rgba(149,20,250,0.1)] w-25 h-25 rounded-full flex justify-center items-center">
-                                <img src={rocketImg} alt="" className=""/>
+                            <div className="flex justify-center">
+                                <div className="bg-linear-to-r from-[rgba(79,57,246,0.1)] to-[rgba(149,20,250,0.1)] w-25 h-25 rounded-full flex justify-center items-center">
+                                    <img src={rocketImg} alt="" className=""/>
+                                </div>
                             </div>
                             <h4 className="text-2xl font-bold">Start Creating</h4>
                             <p className="pb-22 text-[#627382]">Download and start using your premium tools immediately.</p>
